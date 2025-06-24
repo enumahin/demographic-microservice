68a80b1034e0e2665dd6d41d692b5c334b4dea92
enumahin <enumahin@gmail.com>
2025-06-24 16:43:59 +0100

    Complete configuarion implementation

--- src/main/java/com/alienworkspace/cdr/demographic/helpers/Constants.java
+++ src/main/java/com/alienworkspace/cdr/demographic/helpers/Constants.java
@@ -2,14 +2,18 @@ package com.alienworkspace.cdr.demographic.helpers;
 
 /**
  * Class that contains constants used in the application.
- * Created by Ikenumah (enumahinm@gmail.com) on 6/23/2018.
+ * Created by Ikenumah (enumahinm@gmail.com) on 6/23/2025.
  */
 public class Constants {
+
+    public static final String BASE_URL = "/api/demographic";
+
+    public static final String CONFIG_BASE_URL = BASE_URL + "/configs";
     /**
      * Constants used in the application.
      */
-    public static final String PERSON_BASE_URL = "/api/demographic/people";
+    public static final String PERSON_BASE_URL = BASE_URL + "/people";
 
-    public static final String PERSON_ATTRIBUTE_TYPE_BASE_URL = "/api/demographic/person-attribute-types";
+    public static final String PERSON_ATTRIBUTE_TYPE_BASE_URL = BASE_URL + "/person-attribute-types";
 
 }
